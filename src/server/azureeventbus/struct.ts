0d8ac3775995fbc6261db22812dd5a4ef6aaa1b6
vinod kumar kakarla <vinodkakarla564@gmail.com>
2025-04-28 16:44:48 -0700

    refactor: consolidate flight event message payload properties for consistency

--- src/server/azureeventbus/struct.ts
+++ src/server/azureeventbus/struct.ts
@@ -2,12 +2,11 @@ export interface SendFlightEventMessagePayload {
     flight_number: string
     departure_from: string
     arrival_to: string
-    local_departure_date: string
-    local_departure_time: string
-    local_arrival_date: string
-    local_arrival_time: string
-    event_name: string
+    local_departure_date_time: string
+    local_arrival_date_time: string
     event_type: string
+    event_name: string
+    event_time: string
 }
 
 export interface SendFlightEventMessageResponse {
