82df2a9d7a0bc239dcfd4220b1cea66a8a960d47
vinod kumar kakarla <vinodkakarla564@gmail.com>
2025-04-28 16:43:30 -0700

    refactor: update SendFlightEventMessagePayload structure for clarity and accuracy

--- src/server/azureeventbus/struct.ts
+++ src/server/azureeventbus/struct.ts
@@ -1,7 +1,11 @@
 export interface SendFlightEventMessagePayload {
-    flight_id: string
-    status: string
-    reason: string
+    flight_number: string
+    departure_from: string
+    arrival_to: string
+    local_departure_date: string
+    local_departure_time: string
+    local_arrival_date: string
+    local_arrival_time: string
     event_name: string
     event_type: string
 }
